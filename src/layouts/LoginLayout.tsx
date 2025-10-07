36611bcdc03895e9bb601b2f25a4191318ddc1a4
ahmedmustafaux <a.mustafa@cequens.com>
2025-10-07 15:28:47 +0300

    update

--- src/layouts/LoginLayout.tsx
+++ src/layouts/LoginLayout.tsx
@@ -6,7 +6,7 @@ interface LoginLayoutProps {
 
 export default function LoginLayout({ children }: LoginLayoutProps) {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-muted flex flex-col justify-center items-center py-12 px-4 sm:px-6 lg:px-8">
       {children}
     </div>
   )
